b3ac089e6d38678e1c4cb2bd043896eba2a25bd7
Gustavodiasmmm <gustavo.vidal.dias@escola.pr.gov.br>
2026-06-15 10:24:51 -0300

    ubhbhh

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+function scrollToSection() {
+  document.getElementById("sobre").scrollIntoView({ behavior: "smooth" });
+}
 
+// Simples validação do formulário
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Mensagem enviada com sucesso! Entraremos em contato.");
+});
